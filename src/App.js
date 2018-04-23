e91147ccbffa36170125d506da477cdce68d8299
Kalle Laukkanen <kalle.laukkanen@sensofusion.com>
2018-04-23 16:51:27 +0300

    Reproducing symbol click bug

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import ReactMap from 'react-mapbox-gl';
+import ReactMap,{Layer,Feature} from 'react-mapbox-gl';
 
 const accessToken = "pk.eyJ1IjoiYWxleDMxNjUiLCJhIjoiY2o0MHp2cGtiMGFrajMycG5nbzBuY2pjaiJ9.QDApU0XH2v35viSwQuln5w";
 const style = "mapbox://styles/mapbox/streets-v9";
@@ -14,14 +14,38 @@ const mapStyle = {
 };
 
 class App extends Component {
+    constructor(props){
+        super(props);
+        this.state=
+            {
+                updateInterval:null,
+                time: 0
+            }
+    }
+    componentDidMount(){
+        this.setState({updateInterval:setInterval(this.updateTime,1000)})
+    }
+    componentWillUnmount(){
+        clearInterval(this.state.updateInterval)
+    }
+    updateTime=()=>{
+        console.log("Time updated")
+        //let time=Date.now();
+        this.setState({time:Date.now()})
+    }
   render() {
     return (
       <Map
         style={style}
         containerStyle={mapStyle}
-      />
+      >
+        <Layer type="symbol" layout={{"icon-image":"airport-15","icon-size":10}} paint={{"icon-color":"#00FF00"}}>
+            <Feature coordinates={[25,60]} onClick={()=>{console.log("Clicked")}}/>
+        </Layer>
+      </Map>
     );
   }
 }
+
 
 export default App;
